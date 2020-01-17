10883b04e7eb8a31e9e71ad4cc9cc01f89edfc12
Samuel Despain <des14003@byui.edu>
2020-01-17 15:32:58 -0700

    added <p> tags to echos

--- web/mainPage.php
+++ web/mainPage.php
@@ -12,9 +12,9 @@
 		<div>
 			<h2>About me</h2>
 			<?php
-			echo "Hello. I am Samuel, and I am a resident of sunny Gilbert, Arizona. I have not lived there my whole life, though. I had the privilege of living in different climates in different parts of the world.";
-			echo "I was born in Provo, Utah, when my parents were students at BYU. During the first part of my childhood, my family had moved around between Arizona, Colorado, and even Spain.";
-			echo "We settled in Colorado until the end of my freshman year of high school, after which we moved to Arizona where the family currently resides.";
+			echo "<p>Hello. I am Samuel, and I am a resident of sunny Gilbert, Arizona. I have not lived there my whole life, though. I had the privilege of living in different climates in different parts of the world.</p>";
+			echo "<p>I was born in Provo, Utah, when my parents were students at BYU. During the first part of my childhood, my family had moved around between Arizona, Colorado, and even Spain.</p>";
+			echo "<p>We settled in Colorado until the end of my freshman year of high school, after which we moved to Arizona where the family currently resides.</p>";
 			?>
 		</div>
 		<div>
