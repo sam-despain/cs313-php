e34207194cbb2c85d349d7c68213bfb935de5e15
Samuel Despain <des14003@byui.edu>
2020-01-17 17:12:00 -0700

    added content to header.php

---
+++ web/header.php
@@ -0,0 +1,15 @@
+<DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="mainStyle.css"/>
+	</head>
+	<body>
+		<a class="logo" href="index.php">
+			<img width="200px" height="200px" src="logo.svg"/>
+		</a>
+		<div>
+			<a class="headerLink" href="home.php">Home</a>
+			<a class="headerLink">Assignments</a>
+		</div>
+	</body>
+</html>
