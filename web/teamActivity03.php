02e8e2743de5f011c9a9f4933366c6cf4583e268
Samuel Despain <des14003@byui.edu>
2020-01-22 13:07:03 -0700

    edited teamActivity03.php

--- web/teamActivity03.php
+++ web/teamActivity03.php
@@ -1,10 +1,17 @@
 <html>
 	<body>
 		<?php
-		$name = $_POST["name"];
-		$email = $_POST["emailAddress"];
-		$major = $_POST["major"];
-		$comments = $_POST["comments"];
+		$name = testInput($_POST["name"]);
+		$email = testInput($_POST["emailAddress"]);
+		$major = testInput($_POST["major"]);
+		$comments = testInput($_POST["comments"]);
+		
+		function testInput ($data) {
+			$data = trim($data);
+			$data = stripslashes($data);
+			$data = htmlspecialchars($data);
+			return $data;
+		}
 		?>
 		<p>Name: <?php echo $name; ?></p>
 		<p>Email: <a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a></p>
