2344d920843c283b3e91d5e5e6b63defa6880a1f
Samuel Despain <des14003@byui.edu>
2020-01-25 20:05:06 -0700

    testing session functionality

--- web/viewCart.php
+++ web/viewCart.php
@@ -6,21 +6,24 @@ include 'header.html';
 		<h1>Your Cart</h1>
 		<ul>
 		<?php
+		session_start();
+		$_SESSION["cart"]=array();
 		function testInput($data) {
 			$data = trim($data);
 			$data = stripslashes($data);
 			$data = htmlspecialchars($data);
 			return $data;
 		}
-		$cartItems = testInput($_POST["items"]);
+		//$cartItems = testInput($_POST["items"]);	
 		if (isset($_POST["submit"])) {
 			if (!empty($_POST["items"])) {
 				foreach($_POST["items"] as $val) {
+					array_push($_SESSION["cart"],$val);
 					echo "<li>".$val."</li>";
 				}
 			}
 		}
-		echo $cartItems;
+		echo sizeof($_SESSION["cart");
 		?>
 		</ul>
 		<a href="browseItems.php">Back</a>
