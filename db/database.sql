edc9b7e6db9709fb9e0446a40df1b08f000627f8
Samuel Despain <des14003@byui.edu>
2020-01-29 22:48:10 -0700

    created furnitureZone database

---
+++ db/database.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE furnitureZone;
+
+CREATE TABLE furniture (
+	SKU varchar(255),
+	collectionID int,
+	typeID int,
+	width float,
+	height float,
+	depth float,
+	finishID int,
+	doors int,
+	drawers int,
+	FOREIGN KEY (collectionID) REFERENCES collection(ID),
+	FOREIGN KEY (typeID) REFERENCES type(ID),
+	FOREIGN KEY (finishID) REFERENCES finish(ID)
+);
+
+CREATE TABLE collection (
+	ID int,
+	name varchar(255),
+	PRIMARY KEY (ID)
+);
+
+CREATE TABLE type (
+	ID int,
+	name varchar(255),
+	PRIMARY KEY (ID)
+);
+
+CREATE TABLE finish (
+	ID int,
+	name varchar(255),
+	PRIMARY KEY (ID)
+);
