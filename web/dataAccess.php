f857ff13d415f0b255f20362cd963608f2777417
Samuel Despain <des14003@byui.edu>
2020-02-03 23:47:40 -0700

    Testing database access

--- web/dataAccess.php
+++ web/dataAccess.php
@@ -21,6 +21,12 @@ catch (PDOException $ex)
   echo 'Error!: ' . $ex->getMessage();
   die();
 }
+foreach ($db->query('SELECT username, password FROM note_user') as $row)
+{
+  echo 'user: ' . $row['username'];
+  echo ' password: ' . $row['password'];
+  echo '<br/>';
+}
 ?>
 <html>
 	<body>
