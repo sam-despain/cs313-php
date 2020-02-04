11e719863671ffb940a8643d5d9de02b5587f311
Samuel Despain <des14003@byui.edu>
2020-02-04 15:49:32 -0700

    Added select options.

--- web/dataAccess.php
+++ web/dataAccess.php
@@ -2,34 +2,54 @@
 include 'header.html';
 try
 {
-  $dbUrl = getenv('DATABASE_URL');
+	$dbUrl = getenv('DATABASE_URL');
 
-  $dbOpts = parse_url($dbUrl);
+	$dbOpts = parse_url($dbUrl);
 
-  $dbHost = $dbOpts["host"];
-  $dbPort = $dbOpts["port"];
-  $dbUser = $dbOpts["user"];
-  $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/');
+	$dbHost = $dbOpts["host"];
+	$dbPort = $dbOpts["port"];
+	$dbUser = $dbOpts["user"];
+	$dbPassword = $dbOpts["pass"];
+	$dbName = ltrim($dbOpts["path"],'/');
 
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+	$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $ex)
 {
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
-foreach ($db->query('SELECT * FROM finish;') as $row)
-{
-  echo 'ID: ' . $row['id'];
-  echo ' Name: ' . $row['name'];
-  echo '<br/>';
+	echo 'Error!: ' . $ex->getMessage();
+	die();
 }
 ?>
 <html>
 	<body>
-		
+		<select>
+			<option>-Collection-</option>
+			<?php
+			foreach ($db->query('SELECT * FROM collection;') as $row)
+			{
+				echo '<option>' . $row['name'] . '</option>';
+			}
+			?>
+		</select>
+		<select>
+			<option>-Finish-</option>
+			<?php
+			foreach ($db->query('SELECT * FROM finish;') as $row)
+			{
+				echo '<option>' . $row['name'] . '</option>';
+			}
+			?>
+		</select>
+		<select>
+			<option>-Furniture type-</option>
+			<?php
+			foreach ($db->query('SELECT * FROM type;') as $row)
+			{
+				echo '<option>' . $row['name'] . '</option>';
+			}
+			?>
+		</select>
 	</body>
 </html>
