1d7acf66d408fbe81808afc434a7f475189bb4e3
Samuel Despain <des14003@byui.edu>
2020-02-04 23:23:01 -0700

    testing php form

--- web/dataDisplay.php
+++ web/dataDisplay.php
@@ -25,8 +25,7 @@ catch (PDOException $ex)
 <html>
 	<body>
 		<?php
-		echo $_POST["collection"];
-		$myQuery = 'SELECT furniture.sku, furniture.width, furniture.height, furniture.depth, images.link, type.name t, collection.name c, finish.name f FROM images INNER JOIN furniture ON images.id=furniture.imageid INNER JOIN type ON type.id=furniture.typeid INNER JOIN collection ON collection.id = furniture.collectionid INNER JOIN finish ON finish.id = furniture.finishid';
+		$myQuery = 'SELECT furniture.sku, furniture.width, furniture.height, furniture.depth, images.link, type.name t, collection.name c, finish.name f FROM images INNER JOIN furniture ON images.id=furniture.imageid INNER JOIN type ON type.id=furniture.typeid INNER JOIN collection ON collection.id = furniture.collectionid INNER JOIN finish ON finish.id = furniture.finishid WHERE collection.name = \'' . $_POST["collection"] . '\'';
 		foreach ($db->query($myQuery) as $row)
 		{
 			echo '<div>';
