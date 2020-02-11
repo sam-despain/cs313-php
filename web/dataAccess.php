f88e3e6452d2fecb4f023281e2411ee3d1a0724e
Samuel Despain <des14003@byui.edu>
2020-02-11 10:32:05 -0700

    will this fix the bug?

--- web/dataAccess.php
+++ web/dataAccess.php
@@ -60,9 +60,9 @@ catch (PDOException $ex)
 			fr.height,
 			fr.depth,
 			i.link,
-			t.name typeName,
-			c.name collectionName,
-			fi.name finishName FROM images i
+			t.name tname,
+			c.name cname,
+			fi.name fname FROM images i
 			JOIN furniture fr ON i.id = fr.imageid
 			JOIN type t ON t.id = fr.typeid
 			JOIN collection c ON c.id = fr.collectionid
@@ -70,12 +70,12 @@ catch (PDOException $ex)
 		foreach ($db->query($myQuery) as $row)
 		{
 			echo '<div>';
-			echo '<h3>' . $row['typeName'] . '</h3>';
+			echo '<h3>' . $row['tname'] . '</h3>';
 			echo '<img src="' . $row['link'] . '">';
-			echo '<p>SKU: ' . $row['sku'] . '</p>';
+			echo '<p>SKU: #' . $row['sku'] . '</p>';
 			echo '<p>Dimensions: ' . $row['width'] . ' x ' . $row['height'] . ' x ' . $row['depth'] . '</p>';
-			echo '<p>Collection: ' . $row['collectionName'] . '</p>';
-			echo '<p>Finish: ' . $row['finishName'] . '</p>';
+			echo '<p>Collection: ' . $row['cname'] . '</p>';
+			echo '<p>Finish: ' . $row['fname'] . '</p>';
 			echo '</div>';
 		}
 		?>
