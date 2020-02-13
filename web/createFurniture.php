1d8f8e15e8ab14b3b6b2adee9bce96992bc3408a
Samuel Despain <des14003@byui.edu>
2020-02-12 23:28:17 -0700

    testing createFurniture page

--- web/createFurniture.php
+++ web/createFurniture.php
@@ -51,7 +51,7 @@ catch (PDOException $ex)
 		
 		$insertQuery = 'INSERT INTO furniture (typeID, finishID, collectionID, sku, width, height, depth)
 			VALUES (:type_id, :finish_id, :collection_id, :sku, :width, :height, :depth);';
-		$stmt->bindValue(':type_id', $type_id, PDO::PARAM_INT);
+		/*$stmt->bindValue(':type_id', $type_id, PDO::PARAM_INT);
 		$stmt->bindValue(':finish_id', $finish_id, PDO::PARAM_INT);
 		$stmt->bindValue(':collection_id', $collection_id, PDO::PARAM_INT);
 		$stmt->bindValue(':sku', $sku, PDO::PARAM_STR);
@@ -59,7 +59,7 @@ catch (PDOException $ex)
 		$stmt->bindValue(':height', $height, PDO::PARAM_STR);
 		$stmt->bindValue(':depth', $depth, PDO::PARAM_STR);
 		$stmt->execute();
-		//echo $insertQuery;
+		echo $insertQuery;*/
 		?>
 		<p>Creating new furniture...</p>
 	</body>
