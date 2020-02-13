6338672b617d8c72be94e62f63d0e62414a895df
Samuel Despain <des14003@byui.edu>
2020-02-12 17:27:58 -0700

    styling

--- web/dataAccess.php
+++ web/dataAccess.php
@@ -103,31 +103,33 @@ catch (PDOException $ex)
 			</p>
 			<p><input type="submit" value="Search"></p>
 		</form>
-		<?php
-		$myQuery = 'SELECT fr.sku,
-			fr.width,
-			fr.height,
-			fr.depth,
-			i.link,
-			t.name tname,
-			c.name cname,
-			fi.name fname FROM images i
-			JOIN furniture fr ON i.id = fr.imageid
-			JOIN type t ON t.id = fr.typeid
-			JOIN collection c ON c.id = fr.collectionid
-			JOIN finish fi ON fi.id = fr.finishid
-			ORDER BY t.name;';
-		foreach ($db->query($myQuery) as $row)
-		{
-			echo '<div class="furnitureItems">';
-			echo '<h3>' . $row['tname'] . '</h3>';
-			echo '<img src="' . $row['link'] . '">';
-			echo '<p>SKU: #' . $row['sku'] . '</p>';
-			echo '<p>Dimensions: ' . $row['width'] . ' x ' . $row['height'] . ' x ' . $row['depth'] . '</p>';
-			echo '<p>Collection: ' . $row['cname'] . '</p>';
-			echo '<p>Finish: ' . $row['fname'] . '</p>';
-			echo '</div>';
-		}
-		?>
+		<div>
+			<?php
+			$myQuery = 'SELECT fr.sku,
+				fr.width,
+				fr.height,
+				fr.depth,
+				i.link,
+				t.name tname,
+				c.name cname,
+				fi.name fname FROM images i
+				JOIN furniture fr ON i.id = fr.imageid
+				JOIN type t ON t.id = fr.typeid
+				JOIN collection c ON c.id = fr.collectionid
+				JOIN finish fi ON fi.id = fr.finishid
+				ORDER BY t.name;';
+			foreach ($db->query($myQuery) as $row)
+			{
+				echo '<div class="furnitureItems">';
+				echo '<h3>' . $row['tname'] . '</h3>';
+				echo '<img src="' . $row['link'] . '">';
+				echo '<p>SKU: #' . $row['sku'] . '</p>';
+				echo '<p>Dimensions: ' . $row['width'] . ' x ' . $row['height'] . ' x ' . $row['depth'] . '</p>';
+				echo '<p>Collection: ' . $row['cname'] . '</p>';
+				echo '<p>Finish: ' . $row['fname'] . '</p>';
+				echo '</div>';
+			}
+			?>
+		</div>
 	</body>
 </html>
