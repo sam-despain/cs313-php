7b7c2ee7bb47c21f1a6987e9e99e1d5be5ced0a1
Samuel Despain <des14003@byui.edu>
2020-02-12 22:53:31 -0700

    testing createFurniture page

--- web/createFurniture.php
+++ web/createFurniture.php
@@ -27,6 +27,7 @@ catch (PDOException $ex)
 		<?php
 		$typeQuery = 'SELECT id, name FROM type ORDER BY id WHERE name = \'' . htmlspecialchars($_POST['type']) . '\';';
 		$stmt = $db->prepare($typeQuery);
+		$stmt->execute();
 		
 		$finishInput = htmlspecialchars($_POST['finish']);
 		$collectionInput = htmlspecialchars($_POST['collection']);
@@ -37,7 +38,6 @@ catch (PDOException $ex)
 		$depth = htmlspecialchars($_POST['depthInput']);
 		/*
 		
-		$stmt->execute();
 		$typeInput = $stmt->fetchAll(PDO::FETCH_ASSOC);
 		
 		$finishQuery = 'SELECT id, name FROM finish;';
