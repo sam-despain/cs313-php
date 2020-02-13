c4996a36ae01b6347c7dbb0b3c7fdf6edd058ee0
Samuel Despain <des14003@byui.edu>
2020-02-12 23:14:23 -0700

    testing createFurniture page

--- web/createFurniture.php
+++ web/createFurniture.php
@@ -40,8 +40,8 @@ catch (PDOException $ex)
 		$collectionQuery = 'SELECT id, name FROM collection WHERE name = \'' . htmlspecialchars($_POST['collectionInput']) . '\';';
 		$stmt = $db->prepare($collectionQuery);
 		$stmt->execute();
-		/*$collectionInput = $stmt->fetchAll(PDO::FETCH_ASSOC);
-		collection_id = $collectionInput[0]['id'];*/
+		$collectionInput = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		/*collection_id = $collectionInput[0]['id'];*/
 		
 		//$image = htmlspecialchars($_POST['imageInput']);
 		$sku = htmlspecialchars($_POST['skuInput']);
