153b98c3b3a3c331383f3276592ed290b6cb4e27
Samuel Despain <des14003@byui.edu>
2020-02-12 20:56:52 -0700

    testing createFurniture page

--- web/createFurniture.php
+++ web/createFurniture.php
@@ -33,13 +33,13 @@ catch (PDOException $ex)
 		$width = htmlspecialchars($_POST['widthInput']) . "<br/>";
 		$height = htmlspecialchars($_POST['heightInput']) . "<br/>";
 		$depth = htmlspecialchars($_POST['depthInput']) . "<br/>";
-		
+		/*
 		$typeQuery = 'SELECT id, name FROM type;';
 		$stmt = $db->prepare($typeQuery);
 		$stmt->execute();
 		$types = stmt->fetchAll(PDO::FETCH_ASSOC);
 		var_dump($types);
-		/*
+		
 		$finishQuery = 'SELECT id, name FROM finish;';
 		$stmt = $db->prepare($finishQuery);
 		$stmt->execute();
