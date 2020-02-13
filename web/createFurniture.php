f0fcd27f16a6efd8807a2935702891fb6e6c305f
Samuel Despain <des14003@byui.edu>
2020-02-12 21:33:18 -0700

    testing createFurniture page

--- web/createFurniture.php
+++ web/createFurniture.php
@@ -34,23 +34,28 @@ catch (PDOException $ex)
 		$height = htmlspecialchars($_POST['heightInput']) . "<br/>";
 		$depth = htmlspecialchars($_POST['depthInput']) . "<br/>";
 		
-		$typeQuery = 'SELECT id, name FROM type;';
+		$typeQuery = 'SELECT id, name FROM type ORDER BY id;';
 		$stmt = $db->prepare($typeQuery);
 		$stmt->execute();
 		$types = $stmt->fetchAll(PDO::FETCH_ASSOC);
-		var_dump($types);
 		
 		$finishQuery = 'SELECT id, name FROM finish;';
 		$stmt = $db->prepare($finishQuery);
 		$stmt->execute();
 		$finishes = $stmt->fetchAll(PDO::FETCH_ASSOC);
-		var_dump($finishes);
 		
 		$collectionQuery = 'SELECT id, name FROM collection;';
 		$stmt = $db->prepare($collectionQuery);
 		$stmt->execute();
 		$collections = $stmt->fetchAll(PDO::FETCH_ASSOC);
-		var_dump($collections);
+		
+		$type_id = NULL;
+		foreach ($types as $t) {
+			if ($t['name'] == $typeInput) {
+				$type_id = $t['id'];
+			}
+			echo $type_id;
+		}
 		
 		$insertQuery = 'INSERT INTO furniture (typeID, finishID, collectionID, sku, width, height, depth)
 			VALUES (:type, :finish, :collection, :sku, :width, :height, :depth);';
