0564d767484c8a2bfedf02928c81b6a0267d6681
Samuel Despain <des14003@byui.edu>
2020-02-13 22:52:39 -0700

    Simplifying database

--- db/database.sql
+++ db/database.sql
@@ -21,6 +21,7 @@ name varchar(255),
 CREATE TABLE type (
 ID SERIAL PRIMARY KEY,
 name varchar(255),
+image varchar(255)
 );
 
 CREATE TABLE finish (
@@ -33,6 +34,14 @@ ID SERIAL PRIMARY KEY,
 link varchar(255),
 );
 
+INSERT INTO type (name, image) VALUES ('Bookcase', 'bookcase.jpg');
+INSERT INTO type (name, image) VALUES ('Chair', 'chair.jpg');
+INSERT INTO type (name, image) VALUES ('Coffee table', 'coffee_table.jpg');
+INSERT INTO type (name, image) VALUES ('Console', 'console.jpg');
+INSERT INTO type (name, image) VALUES ('Desk', 'desk.jpg');
+INSERT INTO type (name, image) VALUES ('Dresser', 'dresser.jpg');
+INSERT INTO type (name, image) VALUES ('End table', 'end_table.jpg');
+INSERT INTO type (name, image) VALUES ('File cabinet', 'file_cabinet.jpg');
 INSERT INTO furniture (typeID, finishID, collectionID, sku, width, height, depth)
 VALUES ();
 
--- web/dataAccess.php
+++ web/dataAccess.php
@@ -37,26 +37,6 @@ catch (PDOException $ex)
 					?>
 				</select>
 			</p>
-			<p class="fittedDiv" style="padding-right: 20px;">Finish<br>
-				<select name="finishInput">
-					<?php
-					foreach ($db->query('SELECT * FROM finish ORDER BY finish.name;') as $row)
-					{
-						echo '<option>' . $row['name'] . '</option>';
-					}
-					?>
-				</select>
-			</p>
-			<p class="fittedDiv" style="padding-right: 20px;">Collection<br>
-				<select name="collectionInput">
-					<?php
-					foreach ($db->query('SELECT * FROM collection ORDER BY collection.name;') as $row)
-					{
-						echo '<option>' . $row['name'] . '</option>';
-					}
-					?>
-				</select>
-			</p>
 			<p>Dimensions<br>
 			<input type="number" name="widthInput" placeholder="Width">
 			<input type="number" name="heightInput" placeholder="Height">
@@ -65,34 +45,12 @@ catch (PDOException $ex)
 		</form>
 		<form class="fittedDiv" action="dataDisplay.php" method="post">
 			<h2>Search for furniture</h2>
-			<p>SKU #<br><input type="text" name="sku" placeholder="AA0000 or ZAAA-0000"></p>
+			<p>SKU #<br><input type="number" name="sku"></p>
 			<p class="fittedDiv">Furniture type<br>
 				<select name="type">
 					<option></option>
 					<?php
 					foreach ($db->query('SELECT * FROM type ORDER BY type.name;') as $row)
-					{
-						echo '<option>' . $row['name'] . '</option>';
-					}
-					?>
-				</select>
-			</p>
-			<p class="fittedDiv">Finish<br>
-				<select name="finish">
-					<option></option>
-					<?php
-					foreach ($db->query('SELECT * FROM finish ORDER BY finish.name;') as $row)
-					{
-						echo '<option>' . $row['name'] . '</option>';
-					}
-					?>
-				</select>
-			</p>
-			<p class="fittedDiv">Collection<br>
-				<select name="collection">
-					<option></option>
-					<?php
-					foreach ($db->query('SELECT * FROM collection ORDER BY collection.name;') as $row)
 					{
 						echo '<option>' . $row['name'] . '</option>';
 					}
@@ -125,28 +83,6 @@ catch (PDOException $ex)
 					?>
 				</select>
 			</p>
-			<p>NEW Finish<br>
-				<select name="finish">
-					<option></option>
-					<?php
-					foreach ($db->query('SELECT name FROM finish ORDER BY name;') as $row)
-					{
-						echo '<option>' . $row['name'] . '</option>';
-					}
-					?>
-				</select>
-			</p>
-			<p>NEW Collection<br>
-				<select name="collection">
-					<option></option>
-					<?php
-					foreach ($db->query('SELECT name FROM collection ORDER BY name;') as $row)
-					{
-						echo '<option>' . $row['name'] . '</option>';
-					}
-					?>
-				</select>
-			</p>
 			<p><input type="submit" value="Modify"></p>
 		</form>
 		<form class="fittedDiv" action="removeFurniture.php" method="post">
@@ -166,28 +102,17 @@ catch (PDOException $ex)
 		</form>
 		<br>
 		<?php
-		$myQuery = 'SELECT fr.sku,
-			fr.width,
-			fr.height,
-			fr.depth,
-			i.link,
-			t.name tname,
-			c.name cname,
-			fi.name fname FROM furniture fr
-			JOIN images i ON fr.imageid = i.id
-			JOIN type t ON t.id = fr.typeid
-			JOIN collection c ON c.id = fr.collectionid
-			JOIN finish fi ON fi.id = fr.finishid
+		$myQuery = 'SELECT t.name, t.image, f.sku, f.width, f.height, f.depth
+			 FROM furniture f
+			JOIN type t ON t.id = f.typeid
 			ORDER BY t.name;';
 		foreach ($db->query($myQuery) as $row)
 		{
 			echo '<div class="fittedDiv">';
-			echo '<h3>' . $row['tname'] . '</h3>';
-			echo '<img src="' . $row['link'] . '">';
+			echo '<h3>' . $row['name'] . '</h3>';
+			echo '<img src="' . $row['image'] . '">';
 			echo '<p>SKU: #' . $row['sku'] . '</p>';
 			echo '<p>Dimensions: ' . $row['width'] . ' x ' . $row['height'] . ' x ' . $row['depth'] . '</p>';
-			echo '<p>Collection: ' . $row['cname'] . '</p>';
-			echo '<p>Finish: ' . $row['fname'] . '</p>';
 			echo '</div>';
 		}
 		?>
