dfc5f1e8c72fc8affea26b611b348ab24a3fe9c7
Samuel Despain <des14003@byui.edu>
2020-02-13 23:12:12 -0700

    Simplifying database

--- web/createFurniture.php
+++ web/createFurniture.php
@@ -30,32 +30,15 @@ catch (PDOException $ex)
 		$stmt->execute();
 		$typeInput = $stmt->fetchAll(PDO::FETCH_ASSOC);
 		$type_id = $typeInput[0]['id'];
-		
-		$finishQuery = 'SELECT id, name FROM finish WHERE name = \'' . htmlspecialchars($_POST['finishInput']) . '\';';
-		$stmt = $db->prepare($finishQuery);
-		$stmt->execute();
-		$finishInput = $stmt->fetchAll(PDO::FETCH_ASSOC);
-		$finish_id = $finishInput[0]['id'];
-		
-		$collectionQuery = 'SELECT id, name FROM collection WHERE name = \'' . htmlspecialchars($_POST['collectionInput']) . '\';';
-		$stmt = $db->prepare($collectionQuery);
-		$stmt->execute();
-		$collectionInput = $stmt->fetchAll(PDO::FETCH_ASSOC);
-		$collection_id = $collectionInput[0]['id'];
-		
-		$image = 11;
 		$sku = htmlspecialchars($_POST['skuInput']);
 		$width = htmlspecialchars($_POST['widthInput']);
 		$height = htmlspecialchars($_POST['heightInput']);
 		$depth = htmlspecialchars($_POST['depthInput']);
 		
-		$insertQuery = 'INSERT INTO furniture (typeID, finishID, collectionID, imageID, sku, width, height, depth)
-			VALUES (:type_id, :finish_id, :collection_id, :image, :sku, :width, :height, :depth);';
+		$insertQuery = 'INSERT INTO furniture (typeID, sku, width, height, depth)
+			VALUES (:type_id, :sku, :width, :height, :depth);';
 		$stmt = $db->prepare($insertQuery);
 		$stmt->bindValue(':type_id', $type_id, PDO::PARAM_INT);
-		$stmt->bindValue(':finish_id', $finish_id, PDO::PARAM_INT);
-		$stmt->bindValue(':collection_id', $collection_id, PDO::PARAM_INT);
-		$stmt->bindValue(':image', $image, PDO::PARAM_INT);
 		$stmt->bindValue(':sku', $sku, PDO::PARAM_STR);
 		$stmt->bindValue(':width', $width, PDO::PARAM_STR);
 		$stmt->bindValue(':height', $height, PDO::PARAM_STR);
