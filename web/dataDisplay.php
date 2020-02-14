94317a479ac431b09cc5a6b603bd05f10eb80c79
Samuel Despain <des14003@byui.edu>
2020-02-14 12:43:53 -0700

    testing error handling

--- web/dataDisplay.php
+++ web/dataDisplay.php
@@ -32,16 +32,16 @@ include 'back.html';
 		$myQuery = 'SELECT t.name, t.image, f.sku, f.width, f.height, f.depth
 			FROM furniture f
 			JOIN type t ON t.id = f.typeid
-			WHERE name = \'' . $type . '\' OR sku = \'' . $sku . '\'
+			WHERE (name = \'' . $type . '\') OR (sku = ' . $sku . ')
 			ORDER BY t.name;';
 		
 		foreach ($db->query($myQuery) as $row) {
-			//echo '<div class="fittedDiv">';
-			//echo '<h3>' . $row['name'] . '</h3>';
-			//echo '<img src="' . $row['image'] . '">';
-			//echo '<p>SKU: #' . $row['sku'] . '</p>';
-			//echo '<p>Dimensions: ' . $row['width'] . ' x ' . $row['height'] . ' x ' . $row['depth'] . '</p>';
-			//echo '</div>';
+			echo '<div class="fittedDiv">';
+			echo '<h3>' . $row['name'] . '</h3>';
+			echo '<img src="' . $row['image'] . '">';
+			echo '<p>SKU: #' . $row['sku'] . '</p>';
+			echo '<p>Dimensions: ' . $row['width'] . ' x ' . $row['height'] . ' x ' . $row['depth'] . '</p>';
+			echo '</div>';
 		}
 		?>
 		
