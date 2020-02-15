1c9dfd586a3cd814d5d29d6ec719d6b92eb14ea2
Samuel Despain <des14003@byui.edu>
2020-02-14 23:29:22 -0700

    developing modifyFurniture

--- web/modifyFurniture.php
+++ web/modifyFurniture.php
@@ -50,14 +50,14 @@ echo "<h1>Be sure to use a unique SKU number.</h1>";
 			$stmt->bindValue(':sku', $sku, PDO::PARAM_STR);
 			$stmt->bindValue(':newSku', $newSku, PDO::PARAM_STR);
 			$stmt->execute();
-			echo "<p>New SKU</p>";
+			echo "<p>New SKU: " . $sku . "</p>";
 		}
 		if ($type_id != NULL) {
 			$stmt = $db->prepare($modifyQueryT);
 			$stmt->bindValue(':sku', $sku, PDO::PARAM_STR);
 			$stmt->bindValue(':type_id', $type_id, PDO::PARAM_STR);
 			$stmt->execute();
-			echo "<p>New type</p>";
+			echo "<p>New type: " . $typeInput . "</p>";
 		}/*
 		$newPage = "dataAccess.php";
 		header ("Location: $newPage");*/
