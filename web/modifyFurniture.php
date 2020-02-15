3b7866399a1b9f5ab88a752b079705e41c482eb3
Samuel Despain <des14003@byui.edu>
2020-02-14 23:16:31 -0700

    developing modifyFurniture

--- web/modifyFurniture.php
+++ web/modifyFurniture.php
@@ -43,17 +43,16 @@ echo "<h1>Be sure to use a unique SKU number.</h1>";
 		$newHeight = htmlspecialchars($_POST['heightInput']);
 		$newDepth = htmlspecialchars($_POST['depthInput']);
 		
-		$modifyQuery = 'UPDATE furniture SET sku = :newSku WHERE sku = :sku;';
+		$modifyQuerySKU = 'UPDATE furniture SET sku = :newSku WHERE sku = :sku;';
+		$modifyQueryT = 'UPDATE furniture SET typeid = :type_id WHERE sku = :sku;';
 		if ($newSku != NULL) {
-			$stmt = $db->prepare($modifyQuery);
+			$stmt = $db->prepare($modifyQuerySKU);
 			$stmt->bindValue(':sku', $sku, PDO::PARAM_STR);
 			$stmt->bindValue(':newSku', $newSku, PDO::PARAM_STR);
 			$stmt->execute();
 		}
-		echo $type_id;
-		$modifyQuery = 'UPDATE furniture SET typeid = :type_id WHERE sku = :sku;';
 		if ($type_id != NULL) {
-			$stmt = $db->prepare($modifyQuery);
+			$stmt = $db->prepare($modifyQueryT);
 			$stmt->bindValue(':sku', $sku, PDO::PARAM_STR);
 			$stmt->bindValue(':type_id', $type_id, PDO::PARAM_STR);
 			$stmt->execute();
