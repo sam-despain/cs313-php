e9a153addcd03be75517373cafa7323073b948d4
Samuel Despain <des14003@byui.edu>
2020-02-14 23:48:30 -0700

    developing modifyFurniture

--- web/modifyFurniture.php
+++ web/modifyFurniture.php
@@ -56,7 +56,7 @@ echo "<h1>Be sure to use a unique SKU number.</h1>";
 		$modifyQuery .= ' WHERE sku = :sku;';
 		echo $modifyQuery;
 		$stmt = $db->prepare($modifyQuery);
-		$stmt->bindValue(':sku', $sku, PDO::PARAM_STR);
+		$stmt->bindValue(':sku', $sku, PDO::PARAM_INT);
 		$stmt->bindValue(':newSku', $newSku, PDO::PARAM_STR);
 		$stmt->bindValue(':type_id', $type_id, PDO::PARAM_STR);
 		$stmt->execute();
