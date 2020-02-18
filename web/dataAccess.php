6a9cbfe933c1e83860d7764babf647d300769c67
Samuel Despain <des14003@byui.edu>
2020-02-17 23:57:00 -0700

    final touches

--- web/dataAccess.php
+++ web/dataAccess.php
@@ -83,7 +83,7 @@ catch (PDOException $ex)
 		foreach ($db->query($myQuery) as $row)
 		{
 			echo '<div class="fittedDiv">';
-			echo '<h3>' . $row['name'] . '</h3>';
+			echo '<h3 style="text-align: center">' . $row['name'] . '</h3>';
 			echo '<img src="' . $row['image'] . '">';
 			echo '<p>SKU: #' . $row['sku'] . '</p>';
 			echo '<p>Dimensions: ' . $row['width'] . ' x ' . $row['height'] . ' x ' . $row['depth'] . '</p>';
