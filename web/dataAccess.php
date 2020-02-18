2bd75a1350f7f35152d419175d7c1661ced5e63f
Samuel Despain <des14003@byui.edu>
2020-02-17 23:00:42 -0700

    final touches

--- web/dataAccess.php
+++ web/dataAccess.php
@@ -115,15 +115,6 @@ catch (PDOException $ex)
 		foreach ($db->query($myQuery) as $row)
 		{
 			echo '<div class="fittedDiv">';
-			echo '<h3>' . $row['name'] . '</h3>';
-			echo '<img src="' . $row['image'] . '">';
-			echo '<p>SKU: #' . $row['sku'] . '</p>';
-			echo '<p>Dimensions: ' . $row['width'] . ' x ' . $row['height'] . ' x ' . $row['depth'] . '</p>';
-			echo '<button class="dropButton">X</button>';
-			echo '<form class="contents" action="removeFurniture.php" method="post">';
-			echo '<p><input type="hidden" name="sku" value="' . $row['sku'] . '"></p>';
-			echo '<p><input type="submit" value="Remove"></p>';
-			echo '</form>';
 			echo '<button class="dropButton">Edit</button>';
 			echo '<form class="contents" action="modifyFurniture.php" method="post">';
 			echo '<p>SKU #<br>';
@@ -144,6 +135,15 @@ catch (PDOException $ex)
 			echo '<input type="number" name="heightInput" placeholder="Height">';
 			echo '<input type="number" name="depthInput" placeholder="Depth"></p>';
 			echo '<p><input type="submit" value="Modify"></p>';
+			echo '</form>';
+			echo '<h3>' . $row['name'] . '</h3>';
+			echo '<img src="' . $row['image'] . '">';
+			echo '<p>SKU: #' . $row['sku'] . '</p>';
+			echo '<p>Dimensions: ' . $row['width'] . ' x ' . $row['height'] . ' x ' . $row['depth'] . '</p>';
+			echo '<button class="dropButton">X</button>';
+			echo '<form class="contents" action="removeFurniture.php" method="post">';
+			echo '<p><input type="hidden" name="sku" value="' . $row['sku'] . '"></p>';
+			echo '<p><input type="submit" value="Remove"></p>';
 			echo '</form>';
 			echo '</div>';
 		}
