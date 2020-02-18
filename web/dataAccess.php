9a636dcc5bf7e11bbe1f47881b8f3ff848fe2aa9
Samuel Despain <des14003@byui.edu>
2020-02-17 23:24:47 -0700

    final touches

--- web/dataAccess.php
+++ web/dataAccess.php
@@ -87,7 +87,7 @@ catch (PDOException $ex)
 			echo '<img src="' . $row['image'] . '">';
 			echo '<p>SKU: #' . $row['sku'] . '</p>';
 			echo '<p>Dimensions: ' . $row['width'] . ' x ' . $row['height'] . ' x ' . $row['depth'] . '</p>';
-			echo '<button class="dropButton">Edit</button>';
+			echo '<button class="dropButton, fittedDiv">Edit</button>';
 			echo '<form class="contents" action="modifyFurniture.php" method="post">';
 			echo '<p><input type="hidden" name="sku" value="' . $row['sku'] . '"></p>';
 			echo '<p>NEW SKU #<br><input type="number" name="skuInput"></p>';
@@ -105,9 +105,9 @@ catch (PDOException $ex)
 			echo '<input type="number" name="widthInput" placeholder="Width"><br>';
 			echo '<input type="number" name="heightInput" placeholder="Height"><br>';
 			echo '<input type="number" name="depthInput" placeholder="Depth"></p>';
-			echo '<p><input type="submit" value="Save changes"></p>';
+			echo '<p><input type="submit" value="Save"></p>';
 			echo '</form>';
-			echo '<button class="dropButton">Remove</button>';
+			echo '<button class="dropButton, fittedDiv">Remove</button>';
 			echo '<form class="contents" action="removeFurniture.php" method="post">';
 			echo '<p><input type="hidden" name="sku" value="' . $row['sku'] . '"></p>';
 			echo '<p><input type="submit" value="Remove"></p>';
