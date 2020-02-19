c47bd681334b5919429b33de1d8801a2a2a92bc8
Samuel Despain <des14003@byui.edu>
2020-02-19 15:50:12 -0700

    Team activity

---
+++ web/teamActivity07-signup.php
@@ -0,0 +1,34 @@
+<?php
+include 'header.html';
+try
+{
+	$dbUrl = getenv('DATABASE_URL');
+
+	$dbOpts = parse_url($dbUrl);
+
+	$dbHost = $dbOpts["host"];
+	$dbPort = $dbOpts["port"];
+	$dbUser = $dbOpts["user"];
+	$dbPassword = $dbOpts["pass"];
+	$dbName = ltrim($dbOpts["path"],'/');
+
+	$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+	echo 'Error!: ' . $ex->getMessage();
+	die();
+}
+include 'back.html';
+?>
+<html>
+	<body>
+		<form>
+			<input type="text" name="username" placeholder="Username">
+			<input type="password" name="password" placeholder="Password">
+			<input type="password" name="pwrepeat" placeholder="Repeat Password">
+		</form>
+	</body>
+</html>
---
+++ web/teamActivity07.sql
@@ -0,0 +1,5 @@
+CREATE TABLE login (
+	id SERIAL PRIMARY KEY NOT NULL,
+	username varchar(255) NOT NULL,
+	password varchar(255) NOT NULL
+);
